62575fadfa8f7847833e3ba355fac72eabbd1052
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-10-12 16:47:52 -0600

    commit tree 4c7715f2c09ffbef7ee2e42fec7129dd549b20eb

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
+
 function ProjectList({ projects }) {
-  console.log(projects);
+  console.log(projects)
+
+  const proj = projects.map((project) => {
+      return <ProjectItem key={project.id} name={project.name} technologies={project.technologies} about={project.about} />
+    })
+  
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {proj} 
+      </div>
     </div>
   );
 }
